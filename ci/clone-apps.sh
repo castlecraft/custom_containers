88e3a24a9b3ee1972ee72443ea04b11b3f374c95
Revant Nandgaonkar <revant.one@gmail.com>
2022-04-07 22:43:28 +0530

    ci: clone custom apps

--- ci/clone-apps.sh
+++ ci/clone-apps.sh
@@ -1,4 +1,7 @@
 # !/bin/bash
+
+# Use PAT or ssh for private repos
+
 git clone --depth 1 --branch master https://github.com/yrestom/POS-Awesome repos/posawesome
 git clone --depth 1 --branch master https://github.com/zerodha/frappe-attachments-s3 repos/frappe_s3_attachment
 git clone --depth 1 --branch master https://github.com/pipech/frappe-metabase repos/metabase_integration
