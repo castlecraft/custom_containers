904f473851daf966cbd87c7800672ff63611db69
Revant Nandgaonkar <revant.one@gmail.com>
2023-08-29 21:39:09 +0530

    feat: create assets dir for empty sites directory

---
+++ frappe-builder-repo/resources/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Create assets directory if not found
+[ -d "${PWD}/sites/assets" ] || mkdir -p "${PWD}/sites/assets"
+
+# Copy assets*.json from image to assets volume if updated
+cp -uf /opt/frappe/assets/*.json "${PWD}/sites/assets/" 2>/dev/null
+
+# Symlink public directories of app(s) to assets
+find apps -type d -name public | while read -r line; do
+  app_name=$(echo "${line}" | awk -F / '{print $3}')
+  assets_source=${PWD}/${line}
+  assets_dest=${PWD}/sites/assets/${app_name}
+  ln -sf "${assets_source}" "${assets_dest}";
+done
+
+exec "$@"
