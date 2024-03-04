c0162d803cfda621843abba3299d7f88c8b7cb32
Revant Nandgaonkar <revant.one@gmail.com>
2024-03-04 20:25:40 +0530

    fix: entrypoint make paths absolute

--- frappe-builder-repo/resources/entrypoint.sh
+++ frappe-builder-repo/resources/entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Create assets directory if not found
-[ -d "${PWD}/sites/assets" ] || mkdir -p "${PWD}/sites/assets"
+[ -d "/home/frappe/frappe-bench/sites/assets" ] || mkdir -p "/home/frappe/frappe-bench/sites/assets"
 
-# Copy assets*.json from image to assets volume if updated
-cp -uf /opt/frappe/assets/*.json "${PWD}/sites/assets/" 2>/dev/null
+# Copy assets*.json from image to assets volume
+# cp -uf /opt/frappe/assets/*.json "/home/frappe/frappe-bench/sites/assets/" 2>/dev/null
+cp -f /opt/frappe/assets/*.json "/home/frappe/frappe-bench/sites/assets/" 2>/dev/null
 
 # Symlink public directories of app(s) to assets
-find apps -type d -name public | while read -r line; do
-  app_name=$(echo "${line}" | awk -F / '{print $3}')
-  assets_source=${PWD}/${line}
-  assets_dest=${PWD}/sites/assets/${app_name}
-
-  # Create symlink if not found
+find /home/frappe/frappe-bench/apps -type d -name public | while read -r line; do
+  app_name=$(echo "${line}" | awk -F / '{print $(NF-1)}')
+  assets_source=/home/frappe/frappe-bench/${line}
+  assets_dest=/home/frappe/frappe-bench/sites/assets/${app_name}
+  # Create link if not found
   [ -L "${assets_dest}" ] || ln -sf "${assets_source}" "${assets_dest}";
 done
 
